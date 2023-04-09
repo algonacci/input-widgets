c0f86d9f0c8365899ae603966b787d137cf41244
Eric Julianto <algonacci1@gmail.com>
2023-04-09 12:30:41 +0700

    learnt global skill

--- lib/text_form_field.dart
+++ lib/text_form_field.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:email_validator/email_validator.dart';
 
-class TextFormFieldPage extends StatelessWidget {
+class TextFormFieldPage extends StatefulWidget {
   const TextFormFieldPage({super.key});
+
+  @override
+  State<TextFormFieldPage> createState() => _TextFormFieldPageState();
+}
+
+class _TextFormFieldPageState extends State<TextFormFieldPage> {
+  String? _username;
+  String? _email;
+  String? _password;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -16,16 +26,20 @@ class TextFormFieldPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(20),
           child: Form(
+            key: _formKey,
             child: Column(
               children: [
                 TextFormField(
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  initialValue: 'Algonacci',
+                  // autovalidateMode: AutovalidateMode.onUserInteraction,
+                  // initialValue: 'Algonacci',
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Username',
                     hintText: 'Username',
                   ),
+                  onSaved: (value) {
+                    _username = value!;
+                  },
                   validator: (value) {
                     if (value!.length < 6) {
                       return 'Username must be at least 6 characters';
@@ -39,13 +53,16 @@ class TextFormFieldPage extends StatelessWidget {
                 ),
                 TextFormField(
                   keyboardType: TextInputType.emailAddress,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  initialValue: 'Algonacci@gmail.com',
+                  // autovalidateMode: AutovalidateMode.onUserInteraction,
+                  // initialValue: 'Algonacci@gmail.com',
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Email',
                     hintText: 'Email',
                   ),
+                  onSaved: (value) {
+                    _email = value!;
+                  },
                   validator: (email) {
                     if (!EmailValidator.validate(email!)) {
                       return 'Email is not valid!';
@@ -65,6 +82,9 @@ class TextFormFieldPage extends StatelessWidget {
                     labelText: 'Password',
                     hintText: 'Password',
                   ),
+                  onSaved: (value) {
+                    _password = value!;
+                  },
                   validator: (value) {
                     if (value!.length < 8) {
                       return 'Password must be at least 8 characters!';
@@ -72,7 +92,29 @@ class TextFormFieldPage extends StatelessWidget {
                     return null;
                   },
                 ),
-                const SizedBox()
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    bool _validate = _formKey.currentState!.validate();
+                    if (_validate) {
+                      _formKey.currentState!.save();
+                    }
+                    String result =
+                        'username:$_username\nemail:$_email\npassword:$_password';
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          result,
+                        ),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    'Submit',
+                  ),
+                ),
               ],
             ),
           ),
